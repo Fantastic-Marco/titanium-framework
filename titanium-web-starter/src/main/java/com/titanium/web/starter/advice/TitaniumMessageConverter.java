f4228b3e9afa6f838affda999918434468268c6f
dingwei <dingwei@imagedt.com>
2024-07-12 19:00:25 +0800

    feat: 增加Spring-Configuration-metadata.json

--- titanium-web-starter/src/main/java/com/titanium/web/starter/advice/TitaniumMessageConverter.java
+++ titanium-web-starter/src/main/java/com/titanium/web/starter/advice/TitaniumMessageConverter.java
@@ -1,5 +1,6 @@
 package com.titanium.web.starter.advice;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.titanium.common.protocol.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +15,11 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * 用于解决统一响应时，接口声明为String类型
  * 返回类型为String时，会调用{@link org.springframework.http.converter.HttpMessageConverter}
- *
  */
 
 @Slf4j
@@ -27,16 +28,28 @@ public class TitaniumMessageConverter extends MappingJackson2HttpMessageConverte
     public TitaniumMessageConverter(ObjectMapper objectMapper) {
         super();
         setObjectMapper(objectMapper);
+        setSupportedMediaTypes(List.of(
+                MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_JSON_UTF8)
+        );
     }
 
     @Override
     public boolean canRead(Class<?> clazz, MediaType mediaType) {
-        return false;
+        if (ObjectUtil.isNull(mediaType)) {
+            return false;
+        }
+        List<MediaType> mediaTypes = getSupportedMediaTypes(clazz);
+        return mediaTypes.contains(mediaType);
     }
 
     @Override
     public boolean canWrite(Class<?> clazz, MediaType mediaType) {
-        return true;
+        if (ObjectUtil.isNull(mediaType)) {
+            return false;
+        }
+        List<MediaType> mediaTypes = getSupportedMediaTypes(clazz);
+        return mediaTypes.contains(mediaType);
     }
 
     @Override
@@ -47,10 +60,10 @@ public class TitaniumMessageConverter extends MappingJackson2HttpMessageConverte
         if (object instanceof Response) {
             // 已经是统一响应对象，直接调用父类方法序列化
             super.writeInternal(object, type, outputMessage);
-        }  else if(object instanceof ProblemDetail){
+        } else if (object instanceof ProblemDetail) {
             // 如果Controller方法已经返回了ProblemDetail类型，则无需再次包装
             super.writeInternal(object, type, outputMessage);
-        }else {
+        } else {
             // 非统一响应对象，包装后再序列化
             Response<Object> wrappedResponse = Response.ok(object);
             super.writeInternal(wrappedResponse, type, outputMessage);
